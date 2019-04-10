a3220716b7c748f13c97122ead030a7779ffb636
Nicu Pavel <npavel@mini-box.com>
2019-04-09 23:25:09 -0700

    Code style update

--- src/cban.c
+++ src/cban.c
@@ -50,8 +50,7 @@ typedef enum {MRTG, RRDTOOL, CONSOLE_CLEAR, PLAIN} output_format_t;
 static output_format_t output_format = PLAIN;
 
 FILE *f;
-struct statistics 
-{
+struct statistics {
 	int processed;
 	char ifname[255];
 	unsigned long long  last_incoming;
@@ -83,25 +82,24 @@ void help ()
 
 int open_proc_file ()
 {
-    f = fopen (PROCFILE,"r");
-    if (f == NULL)
-    {
-	if (output_format == RRDTOOL){
-	    printf ("U:U\n");
-	    return 0;
-	}
+	f = fopen (PROCFILE,"r");
+	if (f == NULL) {
+		if (output_format == RRDTOOL) {
+			printf ("U:U\n");
+			return 0;
+		}
 
-	fprintf (stderr,"error:unable to open %s for reading.\n", PROCFILE);
-	return 0;
-    }
-    DEBUG("opened %s at %p.\n",PROCFILE, f);
-    return 1;
+		fprintf (stderr,"error:unable to open %s for reading.\n", PROCFILE);
+		return 0;
+	}
+	DEBUG("opened %s at %p.\n",PROCFILE, f);
+	return 1;
 }
 
-void cban_save_current(struct statistics *stat) 
+void cban_save_current(struct statistics *stat)
 {
-    stat->last_incoming = stat->incoming;
-    stat->last_outgoing = stat->outgoing;
+	stat->last_incoming = stat->incoming;
+	stat->last_outgoing = stat->outgoing;
 }
 
 void cban_compute_delta(struct statistics *stat)
@@ -121,7 +119,7 @@ void cban_print(struct statistics *stat)
 {
 	printf( "%s: incoming %s%s/sec: %llu outgoing %s%s/sec: %llu                \n",
 		stat->ifname,
-		kilo, units, stat->delta_incoming*bits/divisor, 
+		kilo, units, stat->delta_incoming*bits/divisor,
 		kilo, units, stat->delta_outgoing*bits/divisor );
 
 }
@@ -142,16 +140,15 @@ int parse_proc_net_dev(void)
 	// skip 2 lines
 	temporary = fgets(buffer, BUFFER, f);
 	temporary = fgets(buffer, BUFFER, f);
-	
-	while(fgets(buffer, BUFFER, f))
-	{
+
+	while(fgets(buffer, BUFFER, f)) {
 		struct statistics *stat = &interfaces[intf_idx];
 		stat->processed = 0;
 
 		ret = sscanf(current, "%[^:]: %llu %*u %*u %*u %*u %*u %*u %*u %llu %*u",
-			stat->ifname, &stat->incoming, &stat->outgoing);
+			     stat->ifname, &stat->incoming, &stat->outgoing);
 
-		if (ret < 3) 
+		if (ret < 3)
 			printf("not all params converted %d!\n", ret);
 		else
 			stat->processed = 1;
@@ -160,7 +157,7 @@ int parse_proc_net_dev(void)
 		intf_idx++;
 		if (intf_idx >= MAX_INTERFACES)
 			break;
-	} 
+	}
 
 	fclose(f);
 	DEBUG("closed %s at %p.\n", PROCFILE, f);
@@ -173,75 +170,70 @@ void monitor_interface()
 {
 	int i;
 	unsigned long long  incoming, outgoing;
-	
+
 	// if we are using format for rrdtool
 	// we must do this before everithig starts
 	// so in a case of error we fail ok.
 	// (we are piped with rrdtool)
-	if (output_format == RRDTOOL){
+	if (output_format == RRDTOOL) {
 		printf ("update %s N:", rrd_filename);
 	}
-	
+
 	// initial state
-	if(!parse_proc_net_dev())
-	{
-	    if (output_format == RRDTOOL){
-		printf ("U:U\n");		
-	    } else {
-		fprintf( stderr, "Error in parse_proc_net_dev() function.\n" );
-	    }
-	    exit(1);
+	if(!parse_proc_net_dev()) {
+		if (output_format == RRDTOOL) {
+			printf ("U:U\n");
+		} else {
+			fprintf( stderr, "Error in parse_proc_net_dev() function.\n" );
+		}
+		exit(1);
 	}
 
 
 	if (output_format > RRDTOOL) {
-	    if (output_format == CONSOLE_CLEAR) {
-		printf("%cc",27);              // reset terminal
-		printf("%c[2J",27);            // clear screeen
-	    }
-
-	    for(i = 0; i < MAX_INTERFACES; i++) 
-	    {
-		struct statistics *stat = &interfaces[i];
-		if (!stat->processed) continue;
-		cban_save_current(stat);
-	    }
-	    
-	    while(1)
-	    {
-		sleep(update);
-		parse_proc_net_dev();
-
-		if (output_format == CONSOLE_CLEAR)
-			printf("%c[H",27); // use escape to put the cursor up
-
-		for(i = 0; i < MAX_INTERFACES; i++) 
-		{
-		    struct statistics *stat = &interfaces[i];
-		    if (!stat->processed) { 
-			continue;
-		    }
-		    cban_compute_delta(stat);
-		    cban_save_current(stat);
-		    cban_print(stat);
+		if (output_format == CONSOLE_CLEAR) {
+			printf("%cc",27);              // reset terminal
+			printf("%c[2J",27);            // clear screeen
 		}
-	    }
+
+		for(i = 0; i < MAX_INTERFACES; i++) {
+			struct statistics *stat = &interfaces[i];
+			if (!stat->processed) continue;
+			cban_save_current(stat);
+		}
+
+		while(1) {
+			sleep(update);
+			parse_proc_net_dev();
+
+			if (output_format == CONSOLE_CLEAR)
+				printf("%c[H",27); // use escape to put the cursor up
+
+			for(i = 0; i < MAX_INTERFACES; i++) {
+				struct statistics *stat = &interfaces[i];
+				if (!stat->processed) {
+					continue;
+				}
+				cban_compute_delta(stat);
+				cban_save_current(stat);
+				cban_print(stat);
+			}
+		}
 	}
 	// just output one set of values for mrtg or rrdtool.
 	// rrdtool use total number of bytes in/out.
 	else {
-	    if (output_format == RRDTOOL)
-	    {
+		if (output_format == RRDTOOL) {
 //		printf( "%llu:%llu\n",
 //		previous.incoming*bits/divisor,
 //		previous.outgoing*bits/divisor );
-	    } else {
+		} else {
 //		sleep(update);
 //		parse_proc_net_dev();
 //		incoming = (current.incoming - previous.incoming) / update * 1000 / 1024;
 //		outgoing = (current.outgoing - previous.outgoing) / update * 1000 / 1024;
 //		printf( "%llu\n%llu\n", incoming*bits/divisor, outgoing*bits/divisor );
-	    }
+		}
 	}
 }
 
@@ -249,11 +241,9 @@ void monitor_interface()
 int main ( int argc, char *argv[] )
 {
 	int option;
-	
-	while (( option = getopt ( argc, argv, "hbkcd:i:u:mr:")) != -1 )
-	{
-		switch (option) 
-		{
+
+	while (( option = getopt ( argc, argv, "hbkcd:i:u:mr:")) != -1 ) {
+		switch (option) {
 		case 'h':
 			help();
 			return 0;
@@ -272,7 +262,7 @@ int main ( int argc, char *argv[] )
 		case 'k':
 			divisor = 1024;
 			kilo = "kilo";
-			break;	
+			break;
 		case 'i':
 			interface = strdup (optarg);
 			DEBUG("Using interface: %s\n", interface);
@@ -289,22 +279,20 @@ int main ( int argc, char *argv[] )
 			rrd_filename = strdup(optarg);
 			debug = 0;
 		}
-	}	
-	if (output_format == RRDTOOL)
-	{
-	    if (!rrd_filename) {
-		fprintf(stderr, "error: you must specify what file to update when using rrdtool option.\n");
-		return 2;
-	    }
+	}
+	if (output_format == RRDTOOL) {
+		if (!rrd_filename) {
+			fprintf(stderr, "error: you must specify what file to update when using rrdtool option.\n");
+			return 2;
+		}
 
-	    if (!interface) {
-		fprintf(stderr, "error: you must specify an interface when using rrdtool output mode.\n");
-		return 3;
-	    }
+		if (!interface) {
+			fprintf(stderr, "error: you must specify an interface when using rrdtool output mode.\n");
+			return 3;
+		}
 
 	}
-	if ( !update ) 
-	{
+	if ( !update ) {
 		DEBUG("warning: using default update interval at %d seconds.\n",UPDATE_INTERVAL);
 		update = UPDATE_INTERVAL;
 	}
